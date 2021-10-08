502f2b677ea852fe0cda2f43496f17bca3bbdbb4
Ben Sullivan <ben@coreillumination.com>
2021-10-08 15:42:34 +1000

    Migrated repo from bitbucket for testing GitHub SCM apps in securestack

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Login from "./components/Login";
+import NavBar from "./components/NavBar";
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
