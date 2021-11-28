71e8e19c9f20f697b0f22732f7aef9c81261fc00
Ben Sullivan <ben@coreillumination.com>
2021-11-28 17:48:56 +1000

    Test actions on existing repo with know vulnerabilities

--- actions/secrets/entrypoint.sh
+++ actions/secrets/entrypoint.sh
@@ -2,6 +2,5 @@
 
 # `$*` expands the `args` supplied in an `array` individually
 # or splits `args` in a string separated by whitespace.
-sh -c "pwd; ls -la"
 sh -c "echo $*"
-sh -c "/bin/bloodhound-cli code -s -v -d 50"
+sh -c "pwd; ls -la;/bin/bloodhound-cli code -s -v -d 50"
