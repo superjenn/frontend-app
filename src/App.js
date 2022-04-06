4f37c0cd9be7461662669cb094b7c03a54f38331
Paul McCarty <mcsnet@yahoo.com>
2022-04-06 14:09:59 +1000

    added password to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import Login from "./components/Login";
 import NavBar from "./components/NavBar";
 function App() {
+
+const password="v6faF+gC7Zs@?VyCqtEYQ%?S"
   return (
     <div className="App">
       <NavBar />
